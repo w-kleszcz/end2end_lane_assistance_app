8a3b09774500ccd530f4172499ec3d3c055a2c3c
Wojciech Kleszcz <wkleszcz@student.agh.edu.pl>
2025-05-21 00:42:05 +0200

    WIP: dataset preparator 2

--- src/app.py
+++ src/app.py
@@ -16,13 +16,38 @@ class Player:
             self.image_paths = []
         else:
             self.image_paths = natsorted(glob.glob(os.path.join(image_folder, "*.jpg")))
-            dpg.configure_item("frame_slider", max_value=len(self.image_paths) - 1)
+            dpg.configure_item(
+                "frame_slider", max_value=len(self.get_valid_frame_indices()) - 1
+            )
             dpg.configure_item("frame_slider", enabled=True)
 
         self.texture_id = texture_id
-        self.frame_delay = 0.033
+        self.frame_delay = self.FRAME_DELAY_DEFAULT
         self.frame_index = 0
         self.playing = False
+
+        self.images_idx_to_skip = []
+        self.previous_slider_index = 0
+
+    def get_valid_frame_indices(self):
+        return [
+            i for i in range(len(self.image_paths)) if i not in self.images_idx_to_skip
+        ]
+
+    def get_number_of_valid_frames(self):
+        return len(self.get_valid_frame_indices())
+
+    def get_slider_index_from_frame_index(self, frame_index):
+        valid_indices = self.get_valid_frame_indices()
+        if frame_index in valid_indices:
+            return valid_indices.index(frame_index)
+        return 0  # fallback
+
+    def get_frame_index_from_slider_index(self, slider_index):
+        valid_indices = self.get_valid_frame_indices()
+        if 0 <= slider_index < len(valid_indices):
+            return valid_indices[slider_index]
+        return 0  # fallback
 
     def load_frame(self, index):
         if index < 0 or index >= len(self.image_paths):
@@ -35,25 +60,41 @@ class Player:
         dpg.set_value(self.texture_id, img.flatten())
 
     def play_loop(self):
-        while self.playing:
+        while self.playing and self.get_number_of_valid_frames() > 0:
             self.load_frame(self.frame_index)
-            self.frame_index = (self.frame_index + 1) % len(self.image_paths)
+            print("Playing frame ", self.frame_index)
+
+            valid_indices = self.get_valid_frame_indices()
+            current_pos = self.get_slider_index_from_frame_index(self.frame_index)
+            next_pos = (current_pos + 1) % len(valid_indices)
+            self.frame_index = valid_indices[next_pos]
+
+            self.set_slider_to_frame_index(self.frame_index)
             time.sleep(self.frame_delay)
 
     def on_folder_selected(self, sender, app_data, user_data):
+        print("Selected folder: ", app_data["file_path_name"])
         self.image_paths = natsorted(
             glob.glob(os.path.join(app_data["file_path_name"], "*.jpg"))
         )
-        dpg.configure_item("frame_slider", max_value=len(self.image_paths) - 1)
+        self.images_idx_to_skip = []
+        self.frame_index = 0
+        dpg.configure_item(
+            "frame_slider", max_value=self.get_number_of_valid_frames() - 1
+        )
+        dpg.set_value("frame_slider", 0)
         dpg.configure_item("frame_slider", enabled=True)
 
     def on_player_slider_change(self, sender, app_data, user_data):
         self.on_pause()
-        self.frame_index = app_data  # app_data is the new slider value
+        print("Raw slider value is ", app_data)
+        self.frame_index = self.get_frame_index_from_slider_index(app_data)
         self.load_frame(self.frame_index)
+        self.previous_slider_index = app_data
+        print("Set frame index via slider to ", self.frame_index)
 
     def on_play(self):
-        if not self.playing:
+        if not self.playing and self.get_number_of_valid_frames() > 0:
             self.playing = True
             threading.Thread(target=self.play_loop, daemon=True).start()
 
@@ -62,54 +103,125 @@ class Player:
 
     def on_step_forward(self):
         self.on_pause()
-        self.frame_index = (self.frame_index + 1) % len(self.image_paths)
+        valid_indices = self.get_valid_frame_indices()
+        cur_idx = self.get_slider_index_from_frame_index(self.frame_index)
+        if cur_idx + 1 < len(valid_indices):
+            self.frame_index = valid_indices[cur_idx + 1]
+        self.set_slider_to_frame_index(self.frame_index)
         self.load_frame(self.frame_index)
 
     def on_step_back(self):
         self.on_pause()
-        self.frame_index = (self.frame_index - 1) % len(self.image_paths)
+        valid_indices = self.get_valid_frame_indices()
+        cur_idx = self.get_slider_index_from_frame_index(self.frame_index)
+        if cur_idx - 1 >= 0:
+            self.frame_index = valid_indices[cur_idx - 1]
+        self.set_slider_to_frame_index(self.frame_index)
         self.load_frame(self.frame_index)
 
     def on_playback_speed_up(self):
-        if self.frame_delay <= 0.001:
-            return
-
-        self.frame_delay *= 0.7
+        if self.frame_delay > 0.001:
+            self.frame_delay *= 0.7
 
     def on_playback_speed_reset(self):
         self.frame_delay = self.FRAME_DELAY_DEFAULT
+
+    def on_remove_frame(self):
+        if (
+            self.frame_index not in self.images_idx_to_skip
+            and self.get_number_of_valid_frames() > 1
+        ):
+            current_valid_idx = self.get_slider_index_from_frame_index(self.frame_index)
+            self.images_idx_to_skip.append(self.frame_index)
+
+            valid_indices = self.get_valid_frame_indices()
+            dpg.configure_item("frame_slider", max_value=len(valid_indices) - 1)
+
+            # Decide new frame index:
+            if current_valid_idx < len(valid_indices):
+                # Go to next valid frame if possible
+                self.frame_index = valid_indices[
+                    min(current_valid_idx, len(valid_indices) - 1)
+                ]
+            else:
+                # If current_valid_idx is out of range (e.g. removed last), step back
+                self.frame_index = valid_indices[-1]
+
+            self.set_slider_to_frame_index(self.frame_index)
+            self.load_frame(self.frame_index)
+
+            print("Currently skipped frames: ", self.images_idx_to_skip)
+            print(
+                "Slider max ", dpg.get_item_configuration("frame_slider")["max_value"]
+            )
+
+    def on_undo_last_frame_remove(self):
+        if len(self.images_idx_to_skip) > 0:
+            self.images_idx_to_skip.pop()
+            dpg.configure_item(
+                "frame_slider", max_value=self.get_number_of_valid_frames() - 1
+            )
+            self.set_slider_to_frame_index(self.frame_index)
+            print("Currently skipped frames: ", self.images_idx_to_skip)
+
+    def set_slider_to_frame_index(self, frame_index):
+        slider_index = self.get_slider_index_from_frame_index(frame_index)
+        dpg.set_value("frame_slider", slider_index)
+
+    def increment_slider(self):
+        slider_val = dpg.get_value("frame_slider")
+        max_val = dpg.get_item_configuration("frame_slider")["max_value"]
+        if slider_val < max_val:
+            dpg.set_value("frame_slider", slider_val + 1)
+
+    def decrement_slider(self):
+        slider_val = dpg.get_value("frame_slider")
+        if slider_val > 0:
+            dpg.set_value("frame_slider", slider_val - 1)
 
 
 class DatasetPreparator(Player):
     def __init__(self, texture_id, image_folder="", image_annotations_file=""):
         super().__init__(texture_id, image_folder)
         self.image_annotations_file = image_annotations_file
-        self.images_idx_to_remove = []
+
         self.test_set_idx_start = None
         self.test_set_idx_finish = None
+
+        with dpg.file_dialog(
+            directory_selector=False,
+            show=False,
+            modal=True,
+            tag="save_file_dialog",
+            callback=self.save_file_with_dataset_metadata,
+            width=400,
+            height=300,
+            default_filename="output.txt",
+            file_count=1,
+            file_dialog_mode=dpg.mvFileDialogMode.Save,
+        ):
+            dpg.add_file_extension(".*")
 
     def set_raw_data_folder(self, raw_data_folder):
         self.raw_data_folder = raw_data_folder
 
-    def set_image_annotations_file(self, image_annotations_file):
-        self.image_annotations_file = image_annotations_file
-
     def on_image_annotations_file_selected(self, sender, app_data, user_data):
+        print("Selected image annotations file: ", app_data["file_path_name"])
         self.image_annotations_file = app_data["file_path_name"]
-
-    def on_remove_frame(self):
-        self.images_idx_to_remove.append(self.frame_index)
-
-    def on_undo_last_frame_remove(self):
-        self.images_idx_to_remove.pop()
 
     def on_test_dataset_start(self):
         self.test_set_idx_start = self.frame_index
+        print("Test set start index set to ", self.test_set_idx_start)
 
     def on_test_dataset_finish(self):
         self.test_set_idx_finish = self.frame_index
+        print("Test set finish index set to ", self.test_set_idx_finish)
 
     def on_save_dataset(path):
+        pass
+
+    def save_file_with_dataset_metadata():
+        dpg.show_item("save_file_dialog")
         pass
 
 
