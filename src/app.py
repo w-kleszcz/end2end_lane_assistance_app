de42108bed11a9f1e94056e9cc4c8143a95091f1
Wojciech Kleszcz <wkleszcz@student.agh.edu.pl>
2025-05-21 23:49:06 +0200

    More beautiful and user friendly gui

--- src/app.py
+++ src/app.py
@@ -88,6 +88,7 @@ class Player:
         )
         dpg.set_value("frame_slider", 0)
         dpg.configure_item("frame_slider", enabled=True)
+        dpg.set_value("images_folder_path", app_data["file_path_name"])
 
     def on_player_slider_change(self, sender, app_data, user_data):
         self.on_pause()
@@ -202,12 +203,28 @@ class DatasetPreparator(Player):
         ):
             dpg.add_file_extension(".yaml")
 
+        with dpg.window(
+            label="Error",
+            modal=True,
+            show=False,
+            tag="error_popup",
+            no_title_bar=True,
+            width=400,
+            height=100,
+        ):
+            dpg.add_text("", tag="error_message_text")  # Placeholder for the message
+            dpg.add_spacer(height=10)
+            dpg.add_button(
+                label="Close", width=75, callback=lambda: dpg.hide_item("error_popup")
+            )
+
     def set_raw_data_folder(self, raw_data_folder):
         self.raw_data_folder = raw_data_folder
 
     def on_image_annotations_file_selected(self, sender, app_data, user_data):
         print("Selected image annotations file: ", app_data["file_path_name"])
         self.image_annotations_file = app_data["file_path_name"]
+        dpg.set_value("annotations_file_path", app_data["file_path_name"])
 
     def on_test_dataset_start(self):
         self.test_set_idx_start = self.frame_index
@@ -217,7 +234,21 @@ class DatasetPreparator(Player):
         self.test_set_idx_finish = self.frame_index
         print("Test set finish index set to ", self.test_set_idx_finish)
 
-    def on_save_dataset(path):
+    def on_save_dataset(self, path):
+        if self.image_annotations_file == "":
+            self.show_error_popup("Please select image annotations file.")
+            return
+
+        if self.images_folder == "":
+            self.show_error_popup("Please select raw images folder.")
+            return
+
+        if self.test_set_idx_start is None or self.test_set_idx_finish is None:
+            self.show_error_popup(
+                "Please set start and finish indices for the test set."
+            )
+            return
+
         dpg.show_item("save_file_dialog")
 
     def save_file_with_dataset_metadata(self, sender, app_data):
@@ -231,11 +262,23 @@ class DatasetPreparator(Player):
         with open(app_data["file_path_name"], "w") as f:
             yaml.dump(data, f, default_flow_style=False)
 
+    def show_error_popup(self, message):
+        dpg.set_value("error_message_text", message)
+        dpg.set_item_pos("error_popup", (150, 150))
+        dpg.show_item("error_popup")
+
+
+APP_WINDOW_WIDTH = 660
+APP_WINDOW_HEIGHT = 770
 
 # ---------- Create texture buffer ----------
 dpg.create_context()
 
-dpg.create_viewport(title="End2end Lane Assistance App", width=800, height=600)
+dpg.create_viewport(
+    title="End2end Lane Assistance App",
+    width=APP_WINDOW_WIDTH,
+    height=APP_WINDOW_HEIGHT,
+)
 
 with dpg.texture_registry():
     player_texture_id = dpg.generate_uuid()
@@ -244,7 +287,11 @@ with dpg.texture_registry():
 
 
 # ---------- GUI Layout ----------
-with dpg.window(label="End2end Lane Assistance App", width=700, height=550):
+with dpg.window(
+    label="End2end Lane Assistance App",
+    width=APP_WINDOW_WIDTH,
+    height=APP_WINDOW_HEIGHT,
+):
     with dpg.tab_bar():
 
         with dpg.tab(label="Data Preparation"):
@@ -258,7 +305,7 @@ with dpg.window(label="End2end Lane Assistance App", width=700, height=550):
                     label="Browse", callback=lambda: dpg.show_item("folder_dialog")
                 )  # Button for browsing folder
                 dpg.add_input_text(
-                    tag="folder_path", width=500, readonly=True
+                    tag="images_folder_path", width=550, readonly=True
                 )  # Textbox for folder path
 
             dpg.add_text("Raw annotations file:")
@@ -267,10 +314,8 @@ with dpg.window(label="End2end Lane Assistance App", width=700, height=550):
                     label="Browse", callback=lambda: dpg.show_item("file_dialog")
                 )
                 dpg.add_input_text(
-                    tag="file_path", width=500, readonly=True
+                    tag="annotations_file_path", width=550, readonly=True
                 )  # Textbox for file path
-
-            dpg.add_button(label="Set raw dataset", callback=None)
 
             # Folder Dialog (for selecting folders)
             with dpg.file_dialog(
@@ -278,6 +323,8 @@ with dpg.window(label="End2end Lane Assistance App", width=700, height=550):
                 show=False,
                 tag="folder_dialog",
                 callback=dataset_preparator.on_folder_selected,
+                width=500,
+                height=400,
             ):
                 dpg.add_file_extension(".*")
 
@@ -287,6 +334,8 @@ with dpg.window(label="End2end Lane Assistance App", width=700, height=550):
                 show=False,
                 tag="file_dialog",
                 callback=dataset_preparator.on_image_annotations_file_selected,
+                width=500,
+                height=400,
             ):
                 dpg.add_file_extension(".txt")
 
